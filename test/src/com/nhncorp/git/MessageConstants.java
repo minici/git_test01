585d249af3bcdd61e3eaeb576f036c74fb34095a
minici <kim.sungmin@nhn.com>
2013-04-01 20:50:45 +0900

    add MesssageConstants Object
    Change-Id: I2c7a3c06a668b7dbb54fe2d7db3877c00e85b080
    Signed-off-by: minici <kim.sungmin@nhn.com>

---
+++ test/src/com/nhncorp/git/MessageConstants.java
@@ -0,0 +1,7 @@
+package com.nhncorp.git;
+
+public class MessageConstants {
+	
+	public static final String MESSAGE_WELCOME = "hello~";
+
+}
--- test/src/com/nhncorp/git/Test.java
+++ test/src/com/nhncorp/git/Test.java
@@ -2,14 +2,12 @@ package com.nhncorp.git;
 
 public class Test {
 	
-	private static final String MESSAGE_HELLO = "hello";
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println(MESSAGE_HELLO);
+		System.out.println(MessageConstants.MESSAGE_WELCOME);
 
 	}
 
