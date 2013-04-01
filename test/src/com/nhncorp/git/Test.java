9779b1839254dc8138453c44eb4797a8e5cbbc0d
minici <kim.sungmin@nhn.com>
2013-04-01 20:44:23 +0900

    add message constant
    Change-Id: I71a0dcec1f9410db3813c15c56fc7a8de4b76312
    Signed-off-by: minici <kim.sungmin@nhn.com>

--- test/src/com/nhncorp/git/Test.java
+++ test/src/com/nhncorp/git/Test.java
@@ -1,13 +1,15 @@
 package com.nhncorp.git;
 
 public class Test {
+	
+	private static final String MESSAGE_HELLO = "hello";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("hi");
+		System.out.println(MESSAGE_HELLO);
 
 	}
 
